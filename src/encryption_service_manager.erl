df97b7eb6458906139c32ecb2932151aa985c510
Artem Stemkovski <astemkov@gmail.com>
2016-08-16 01:08:07 -0700

    MB-4114 base implementation of encryption service
    limitations:
    1. Supports only single data_key
    2. Gets password from env variable only
    3. Doesn't authenticate decrypt() calls
    
    Change-Id: I034a58bda80c459d4c33ac8c48234c4cac11adb2

---
+++ src/encryption_service_manager.erl
@@ -0,0 +1,71 @@
+%% @author Couchbase <info@couchbase.com>
+%% @copyright 2016 Couchbase, Inc.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%      http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%
+%% Responsible for:
+%% 1. Updating encryption service with latest encrypted_data_keys
+%%
+-module(encryption_service_manager).
+
+-behavior(gen_server).
+
+-include("ns_common.hrl").
+
+-export([start_link/0]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+init([]) ->
+    Self = self(),
+    ns_pubsub:subscribe_link(ns_config_events, fun config_change_detector/2, Self),
+    case ns_config:search(encrypted_data_key) of
+        false ->
+            ?log_debug("Retrieve created encrypted data key"),
+            {ok, DataKey} = encryption_service:get_encrypted_data_key(),
+            ns_config:set(encrypted_data_key, DataKey);
+        {value, _} ->
+            Self ! notify_service
+    end,
+    {ok, []}.
+
+config_change_detector({encrypted_data_key, _}, Parent) ->
+    Parent ! notify_service,
+    Parent;
+config_change_detector(_, Parent) ->
+    Parent.
+
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+handle_call(_, _From, State) ->
+    {reply, unknown_call, State}.
+
+handle_cast(_, State) ->
+    {noreply, State}.
+
+handle_info(notify_service, State) ->
+    misc:flush(notify_service),
+    {value, Key} = ns_config:search(encrypted_data_key),
+    encryption_service:set_encrypted_data_key(Key),
+    {noreply, State};
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
