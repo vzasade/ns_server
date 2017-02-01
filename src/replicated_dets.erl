263a52fd4d8a6f078241821b378c000808d36624
Artem Stemkovski <astemkov@gmail.com>
2017-02-01 15:30:34 -0800

    ability to select documents from replicated_dets
    - guarantees that no updates will be commited to dets while select
      is in process
    
    Change-Id: I5e627d5dc368d0f43b666bbfed3820c1f0c2e61d

--- src/replicated_dets.erl
+++ src/replicated_dets.erl
@@ -17,9 +17,12 @@
 
 -module(replicated_dets).
 
+-include("ns_common.hrl").
+-include("pipes.hrl").
+
 -behaviour(replicated_storage).
 
--export([start_link/5, set/3, delete/2, get/2, get/3]).
+-export([start_link/5, set/3, delete/2, get/2, get/3, select/3]).
 
 -export([init/1, init_after_ack/1, handle_call/3,
          get_id/1, find_doc/2, get_all_docs/1,
@@ -36,7 +39,7 @@
               value :: term()}).
 
 start_link(ChildModule, InitParams, Name, Path, Replicator) ->
-    replicated_storage:start_link(Name, ?MODULE, [ChildModule, InitParams, Path, Replicator], Replicator).
+    replicated_storage:start_link(Name, ?MODULE, [Name, ChildModule, InitParams, Path, Replicator], Replicator).
 
 set(Name, Id, Value) ->
     gen_server:call(Name, {interactive_update, #doc{id = Id,
@@ -61,10 +64,16 @@ get(Name, Id, Default) ->
             Value
     end.
 
-init([ChildModule, InitParams, Path, Replicator]) ->
+select(Name, KeySpec, N) ->
+    DocSpec = #doc{id = KeySpec, deleted = false, _ = '_'},
+    MatchSpec = [{DocSpec, [], ['$_']}],
+    ?make_producer(select_from_dets(Name, MatchSpec, N, ?yield())).
+
+init([Name, ChildModule, InitParams, Path, Replicator]) ->
     replicated_storage:anounce_startup(Replicator),
     ChildState = ChildModule:init(InitParams),
-    #state{path = Path,
+    #state{name = Name,
+           path = Path,
            child_module = ChildModule,
            child_state = ChildState}.
 
@@ -72,29 +81,30 @@ init_after_ack(State) ->
     ok = open(State),
     State.
 
-open(#state{path = Path, name = Name}) ->
-    {ok, Name} = dets:open_file(Name, [{type, set},
-                                       {auto_save,3},
-                                       {keypos, #doc.id},
-                                       {file, Path}]),
+open(#state{path = Path, name = TableName}) ->
+    {ok, TableName} =
+        dets:open_file(TableName, [{type, set},
+                                   {auto_save,3},
+                                   {keypos, #doc.id},
+                                   {file, Path}]),
     ok.
 
 get_id(#doc{id = Id}) ->
     Id.
 
-find_doc(Id, #state{name = Name}) ->
-    case dets:lookup(Name, Id) of
+find_doc(Id, #state{name = TableName}) ->
+    case dets:lookup(TableName, Id) of
         [Doc] ->
             Doc;
         [] ->
             false
     end.
 
-get_all_docs(#state{name = Name}) ->
+get_all_docs(#state{name = TableName}) ->
     %% TODO to be replaced with something that does not read the whole thing to memory
     dets:foldl(fun(Doc, Acc) ->
                        [Doc | Acc]
-               end, [], Name).
+               end, [], TableName).
 
 get_revision(#doc{rev = Rev}) ->
     Rev.
@@ -106,15 +116,15 @@ is_deleted(#doc{deleted = Deleted}) ->
     Deleted.
 
 save_doc(#doc{id = Id} = Doc,
-         #state{name = Name,
+         #state{name = TableName,
                 child_module = ChildModule,
                 child_state = ChildState} = State) ->
-    ok = dets:insert(Name, [Doc]),
+    ok = dets:insert(TableName, [Doc]),
     NewChildState = ChildModule:on_save(Id, ChildState),
     {ok, State#state{child_state = NewChildState}}.
 
-handle_call({get, Id}, _From, #state{name = Name} = State) ->
-    RV = case dets:lookup(Name, Id) of
+handle_call({get, Id}, _From, #state{name = TableName} = State) ->
+    RV = case dets:lookup(TableName, Id) of
              [#doc{id = Id, deleted = false, value = Value}] ->
                  {Id, Value};
              [#doc{id = Id, deleted = true}] ->
@@ -122,4 +132,44 @@ handle_call({get, Id}, _From, #state{name = Name} = State) ->
              [] ->
                  false
          end,
-    {reply, RV, State}.
+    {reply, RV, State};
+handle_call(suspend, {Pid, _} = From, #state{name = TableName} = State) ->
+    MRef = erlang:monitor(process, Pid),
+    ?log_debug("Suspended by process ~p", [Pid]),
+    gen_server:reply(From, {ok, TableName}),
+    receive
+        {'DOWN', MRef, _, _, _} ->
+            ?log_info("Suspending process ~p died", [Pid]),
+            {noreply, State};
+        release ->
+            ?log_debug("Released by process ~p", [Pid]),
+            erlang:demonitor(MRef, [flush]),
+            {noreply, State}
+    end.
+
+select_from_dets(Name, MatchSpec, N, Yield) ->
+    {ok, TableName} = gen_server:call(Name, suspend, infinity),
+    dets:safe_fixtable(TableName, true),
+    do_select_from_dets(TableName, MatchSpec, N, Yield),
+    dets:safe_fixtable(Name, false),
+    Name ! release,
+    ok.
+
+do_select_from_dets(TableName, MatchSpec, N, Yield) ->
+    case dets:select(TableName, MatchSpec, N) of
+        {Selection, Continuation} when is_list(Selection) ->
+            do_select_from_dets_continue(Selection, Continuation, Yield);
+        '$end_of_table' ->
+            ok
+    end.
+
+do_select_from_dets_continue(Selection, Continuation, Yield) ->
+    lists:foreach(fun (#doc{id = Id, value = Value}) ->
+                          Yield({Id, Value})
+                  end, Selection),
+    case dets:select(Continuation) of
+        {Selection2, Continuation2} when is_list(Selection2) ->
+            do_select_from_dets_continue(Selection2, Continuation2, Yield);
+        '$end_of_table' ->
+            ok
+    end.
