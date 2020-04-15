3e8438ac8b6412fda2ce6c137a932279e2060601
Artem Stemkovski <astemkov@gmail.com>
2020-04-15 16:34:47 -0700

    MB-38800 correct permission check for POST /_uistats
    Change-Id: I25464c8b905452a27a37ff3a05c6fe594f444d15

--- src/menelaus_stats.erl
+++ src/menelaus_stats.erl
@@ -3083,7 +3083,7 @@ prepare_ui_stats(SamplesForStatsAndNodes) ->
 ui_stats_post_validators(Req) ->
     Now = os:system_time(millisecond),
     [validator:string(bucket, _),
-     validate_bucket(bucket, _),
+     validate_bucket(bucket, Req, _),
      validator:required(stats, _),
      validator:string_array(stats, _),
      validator:string(statName, _),
@@ -3118,15 +3118,16 @@ validate_negative_ts(Name, Now, State) ->
               ok
       end, Name, State).
 
-validate_bucket(Name, State) ->
+validate_bucket(Name, Req, State) ->
     validator:validate(
       fun (BucketName) ->
-              case lists:member(BucketName,
-                                ns_bucket:get_bucket_names()) of
-                  true ->
+              case check_bucket(BucketName, Req) of
+                  ok ->
                       ok;
-                  false ->
-                      {error, "Bucket not found"}
+                  not_found ->
+                      {error, "Bucket not found"};
+                  {forbidden, Permission} ->
+                      {error, {403, Permission}}
               end
       end, Name, State).
 
