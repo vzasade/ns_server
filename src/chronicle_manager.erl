6e752b520216e435194c19b7d23b1b00cc18993c
Artem Stemkovski <astemkov@gmail.com>
2020-08-10 13:09:41 -0700

    forward chronicle events to ns_config_events so existing code
    ...doesn't break after keys are migrated to chronicle
    
    Change-Id: I3a80add524097feeaa7bb2e6c2cbd2aa7754b9e8

--- src/chronicle_manager.erl
+++ src/chronicle_manager.erl
@@ -15,30 +15,75 @@
 %%
 -module(chronicle_manager).
 
+-behaviour(gen_server2).
+
 -include("ns_common.hrl").
+-include("ns_config.hrl").
 -include("cut.hrl").
 
--export([bootstrap/0, rename/0, leave/0, do_join_node/1, join_node/1,
+-export([start_link/0,
+         init/1,
+         handle_call/3,
+         rename/0,
+         leave/0,
+         join_node/1,
          remove_node/1]).
 
-bootstrap() ->
+start_link() ->
+    gen_server2:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+init([]) ->
     application:set_env(chronicle, data_dir,
                         path_config:component_path(data, "config")),
     ok = application:ensure_started(chronicle, permanent),
+
     ok = maybe_provision(),
-    ignore.
+    {ok, subscribe_to_events()}.
 
-leave() ->
-    ok = wipe(),
-    maybe_provision().
-
-rename() ->
+handle_call(leave, _From, Pid) ->
+    ?log_debug("Handle leaving cluster"),
+    {reply, ok, reprovision(Pid)};
+handle_call(rename, _From, Pid) ->
+    ?log_debug("Handle renaming"),
     List = chronicle_kv:submit_query(kv, get_snapshot, 10000, #{}),
-    leave(),
+    NewPid = reprovision(Pid),
     lists:foreach(
       fun ({kv, Key, Value, _Revision}) ->
               chronicle_kv:add(kv, Key, Value)
-      end, List).
+      end, List),
+    {reply, ok, NewPid};
+handle_call({add_node, Node}, _From, Pid) ->
+    ?log_debug("Adding node ~p", [Node]),
+    chronicle:add_voters([Node]),
+    {reply, ok, Pid};
+handle_call({join_node, Node}, _From, Pid) ->
+    ok = wipe(Pid),
+    ?log_debug("Joining to ~p", [Node]),
+    gen_server2:call({?MODULE, Node}, {add_node, node()}),
+    {reply, ok, subscribe_to_events()}.
+
+leave() ->
+    gen_server2:call(?MODULE, leave).
+
+rename() ->
+    gen_server2:call(?MODULE, rename).
+
+join_node(Node) ->
+    gen_server2:call(?MODULE, {join_node, Node}).
+
+reprovision(Pid) ->
+    ok = wipe(Pid),
+    ok = maybe_provision(),
+    subscribe_to_events().
+
+subscribe_to_events() ->
+    ns_pubsub:subscribe_link(
+      chronicle_kv:event_manager(kv),
+      fun ({{key, Key}, _Rev, {updated, Value}}) ->
+              gen_event:notify(ns_config_events, {Key, Value});
+          ({{key, Key}, _Rev, deleted}) ->
+              gen_event:notify(ns_config_events, {Key, ?DELETED_MARKER})
+      end).
 
 maybe_provision() ->
     case chronicle_agent:get_metadata() of
@@ -50,18 +95,10 @@ maybe_provision() ->
             ok
     end.
 
-wipe() ->
+wipe(Pid) ->
     ?log_debug("Wiping the config"),
+    ns_pubsub:unsubscribe(Pid),
     chronicle_agent:wipe().
-
-do_join_node(Node) ->
-    ?log_debug("Adding node ~p", [Node]),
-    chronicle:add_voters([Node]).
-
-join_node(Node) ->
-    ok = wipe(),
-    ?log_debug("Joining to ~p", [Node]),
-    rpc:call(Node, ?MODULE, do_join_node, [node()], 20000).
 
 remove_node(Node) ->
     ?log_debug("Removing node ~p", [Node]),
